32a2c4d2fb3aca8bb0688ebdf07e11428b30ce8e
vijayaprakash2610 <vijayaprakash6491295@gmail.com>
2024-10-22 11:12:43 +0530

    initial commit

---
+++ my-portfolio/src/App.js
@@ -0,0 +1,45 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <center>
+     <h1>VIJAYAPRAKASH</h1>
+     <h2>GRAPHICS DESIGNER</h2><hr></hr>
+     <h2>CONTACTS</h2>
+     <h4>Mail Id:vijayaprakash6491295@gmail.com</h4>
+     <h4>Phone:+91 9566757450 </h4>
+     <h4>Home:Mayiladudhurai,tamilnadu.</h4>
+     <h4>Github:https://github.com/vijayaprakash2610</h4>
+     <h4>Linkedin:vijayaprakash s</h4>
+     </center><hr></hr>
+     <h2>ABOUT ME</h2>
+     <center></center>
+     <p>Hii I'am a passionate graphic designer with a fresh perspective and a keen eye for detail.i love creating visually compelling designs that communicate ideas effectively. with a background in Bachelor of Engineering in computer science,I'm Skilled in using tools like Adobe creative suite and am always eager to learn new techniques. I thrive on collaboration and am excited to bring creativity to every project.lets's make something amazing together! </p>
+     <center></center><hr></hr>
+     <h2>EDUCATION HISTORY</h2>
+     <h3>VETRI VINAYAHA COLLEGE OF ENGINEERING AND TECHNOLOGY</h3>
+     <h3>Bachelor of Engineering </h3>
+     <h4>Department of Computer Science And Engineering</h4>
+     <h4>2021-2025</h4><hr></hr>
+     <h2>SKILLS SUMMARY</h2>
+     <u>
+     <li>HTML</li>
+     <li>CSS</li>
+     <li>JAVA</li>
+     <li> Adobe illustrater</li>
+     </u><hr></hr>
+     <h2>CERTIFICATION</h2>
+     <h4>Artificial intelligence</h4>
+     <h4>Novitech R&D Private Limitted</h4><hr></hr>
+     <h2>EXTRACURICULAR ACTIVITIES</h2>
+     <h4>QUICK LEARNER</h4>
+     <h4>KABADDI</h4>
+
+
+    </div>
+  );
+}
+
+export default App;
